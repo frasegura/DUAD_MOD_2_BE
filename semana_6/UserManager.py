12b74ac36d48a5dc4fae00c1fc8449b030259fdc
frasegura <franciscosegura29@gmail.com>
2025-11-16 02:11:37 -0600

    class UserManger created

---
+++ semana_6/UserManager.py
@@ -0,0 +1,41 @@
+from sqlalchemy import insert, select,update, delete
+from database_setup import engine, users_table
+
+class UserManager:
+    def __init__(self, engine):
+        self.engine = engine
+
+    #Create User
+    def create_user(self, name):
+        stmt = insert(users_table).values(name=name)
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            conn.commit()
+
+    #Update user
+    def update_user(self,user_id, new_name):
+        stmt = (
+            update(users_table)
+            .where(users_table.c.id == user_id)
+            .values(name=new_name)
+        )
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            conn.commit()
+
+    #Delete user
+    def delete_user(self, user_id):
+        stmt = (
+            delete(users_table)
+            .where(users_table.c.id == user_id)
+        )
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            conn.commit()
+
+    #Get all users 
+    def get_all_users(self):
+        stmt = select(users_table)
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            return result.fetchall()
---
+++ semana_6/database_setup.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine,MetaData,Table,Column,Integer,String,ForeignKey
+
+metadata_obj = MetaData()
+
+# DB declaration : tables for users, directions and cars
+
+users_table = Table(
+    "users",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name",String(30))
+)
+
+addresses_table = Table(
+    "addresses",
+    metadata_obj,
+    Column("id",Integer, primary_key=True),
+    Column("street", String(100)),
+
+    # FK
+    Column("user_id",Integer,ForeignKey("users.id"),nullable=False)
+)
+
+cars_table = Table(
+
+    "cars",
+    metadata_obj,
+    Column("id",Integer, primary_key=True),
+    Column("model",String(50)),
+
+    #FK
+    Column("users_id",Integer, ForeignKey("users.id"), nullable=True)
+)
+
+
+#Create connection
+DB_URI = 'postgres://postgres:postgres@localhost:5432/postgres'
+engine = create_engine(DB_URI, echo= True)
+
+try:
+    connection = engine.connect()
+    print("Connection successfull")
+    connection.close()
+except Exception as e:
+    print("Connection failed :", e) 
+
+
+#Table validation: tables will be created if they do not exist. 
+
+metadata_obj.create_all(engine)
+print("Tables are validated, and created if they do not exist.")
+
