1a9d4843d5b1f178644390888b6ff62baeb103ad
frasegura <franciscosegura29@gmail.com>
2025-11-16 01:20:51 -0600

    tables declaration

---
+++ semana_6/data_tables.py
@@ -0,0 +1,32 @@
+from sqlalchemy import MetaData,Table,Column,Integer,String,ForeignKey
+
+metadata_obj = MetaData()
+
+# DB table for users, directions and cars
+
+users_table = Table(
+    "users",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name",String(30))
+)
+
+addresses_table = Table(
+    "addresses",
+    metadata_obj,
+    Column("id",Integer, primary_key=True),
+    Column("street", String(100)),
+
+    # FK
+    Column("user_id",Integer,ForeignKey("users.id"),nullable=False)
+)
+
+cars_table = Table(
+    "cars",
+    metadata_obj,
+    Column("id",Integer, primary_key=True),
+    Column("model",String(50)),
+
+    #FK
+    Column("users_id",Integer, ForeignKey("users.id"), nullable=True)
+)
